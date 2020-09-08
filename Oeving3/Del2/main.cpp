f69058a5d587dae3dde12044c38e47099cd5b14f
Ingebrigt <ing.hovind@hotmail.com>
2020-09-08 11:55:04 +0200

    ferdig

--- Oeving3/Del2/main.cpp
+++ Oeving3/Del2/main.cpp
@@ -2,48 +2,19 @@
 #include "binaryTree.cpp"
 #include <cmath>
 
-int main()
+int main(int argc, char *argv[])
 {
     using namespace std;
     static binaryTree* root2 = NULL;
-    //side 123
-    //https://www.geeksforgeeks.org/level-order-tree-traversal/
-    string words;
-    cout << "Enter words separated by a space:\n";
-    //cin >> words;
-    string tempword = "";
     binaryTree* root;
-    root = insertNode(NULL, "hode");
-    insertNode(root, "bein");
-    insertNode(root, "hals");
-    insertNode(root, "arm");
-    insertNode(root, "tann");
-    insertNode(root, "hånd");
-    insertNode(root, "tå");
-    string test = "hånd";
-    string test1 = "hond";
-    //cout << (test > test1) <<"\n";
-    //cout << root->rightChild->leftChild->leftChild->value << "\n";
+
+    root = insertNode(NULL, argv[1]);
+
+    for(int i = 2; i< argc; i++){
+        insertNode(root, argv[i]);
+    }
+
     for(int i = 1; i<5; i++){
         cout << printGivenLevel(root,i, 64) << "\n";
     }
-    /*
-    //method to enter words and insert them into the tree
-    string display = "";
-    for(int i = 1; i<5; i++){
-        //cout << 64/pow(2,i-1) << "\n";
-        int j;
-        for(int k = 0; k < printGivenLevel(root, i).size();k++){
-            j = 64/pow(2,i-1);
-            display += multiplyString(" ", (j/2));
-            display += printGivenLevel(root,i).at(k);
-            display += multiplyString(" ", (j/2));
-        }
-        display += "\n";
-        //display += " \n";
-    }
-    cout << display;
-     */
 }
-
-
