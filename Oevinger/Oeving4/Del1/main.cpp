19cdf2aa5f9a2fa51f0bab5b38be606649fe38d9
Ingebrigt <ing.hovind@hotmail.com>
2020-09-18 12:57:15 +0200

    hash function

--- Oevinger/Oeving4/Del1/main.cpp
+++ Oevinger/Oeving4/Del1/main.cpp
@@ -9,7 +9,8 @@ struct placedLink{
     unsigned char name[];
 };
 using namespace std;
-unsigned long myHash(unsigned char str[]);
+unsigned long multhash(int k, int x);
+unsigned long keyGen(unsigned char *str);
 #define MAXCHAR 1000
 int main() {
     using namespace std;
@@ -21,21 +22,38 @@ int main() {
     if (fp == NULL){
         return 1;
     }
+    int counter = 0;
+    unsigned long max = 0;
+    unsigned long min = 7963063666499351619;
     while (fgets(str, MAXCHAR, fp) != NULL)
     {
+        if(max < keyGen(reinterpret_cast<unsigned char *>(str))){
+            max = keyGen(reinterpret_cast<unsigned char *>(str));
+        }
+        if(min > keyGen(reinterpret_cast<unsigned char *>(str))){
+            min = keyGen(reinterpret_cast<unsigned char *>(str));
+        }
         struct placedLink name = {NULL, NULL};
         memcpy(name.name,str, sizeof(str));
-        printf("%lu\n",myHash(name.name));
+        printf("%lu\n", multhash(keyGen(name.name),7));
+        counter++;
     }
+    printf("\nmin: %lu", min);
+    printf("\nmax: %lu", max);
     fclose(fp);
     return 0;
 }
 
-unsigned long myHash(unsigned char str[]){
+unsigned long keyGen(unsigned char *str){
     unsigned long sum = 0;
     for(int i = 0; i < strlen(reinterpret_cast<const char *>(str)); i++){
         //ganger med  7^i
         sum = sum * 7+ (str[i])*(7);
     }
     return sum;
+}
+
+unsigned long multhash(int k, int x){
+    const std::uint32_t knuth = 2654435769;
+    return k * knuth >> (32-x);
 }
