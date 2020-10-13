40722b65de4a44f6b36a8ddebe5cbf6116167eaa
Ingebrigt <ing.hovind@hotmail.com>
2020-10-13 20:14:51 +0200

    todo

--- Oevinger/Oeving7/src/Huffmain.java
+++ Oevinger/Oeving7/src/Huffmain.java
@@ -10,6 +10,7 @@ import java.nio.file.Path;
 
 public class Huffmain {
     public static void main(String args[]) throws IOException {
+        //TODO SE PÅ KODEN HAN HAR LAGT UT PÅ OPPGAVEN OG SE OM DEN HJELPER
         //får problemer med •
         //todo bytte til unicode?
         File input = new File("files/diverse.txt");
