4f6b6da97833edf44de70db916ba86e9667d0147
Ingebrigt <ing.hovind@hotmail.com>
2020-10-14 12:20:20 +0200

    man enkoder bytes ikke karakterer, det var derfor jeg fikk funky bytes

--- Oevinger/Oeving7/src/Huffmain.java
+++ Oevinger/Oeving7/src/Huffmain.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,13 +14,12 @@ public class Huffmain {
         //TODO SE PÅ KODEN HAN HAR LAGT UT PÅ OPPGAVEN OG SE OM DEN HJELPER
         //får problemer med •
         //todo bytte til unicode?
-        File input = new File("files/diverse.txt");
+        File input = new File("files/output.txt");
         File output = new File("files/output.txt");
         int[] frequencyArray = getFrequencyArray(input.getPath());
         for(int i = 0; i < frequencyArray.length; i++){
             if(frequencyArray[i] > 0){
-
-               System.out.println(((char) i )  + " " + i + ": " + frequencyArray[i]);
+                System.out.println(i + ": " + frequencyArray[i]);
             }
         }
 
@@ -38,12 +38,19 @@ public class Huffmain {
     public static int[] getHuffmanTree(){
         return null;
     }
+
+    /**
+     * Denne lagrer antall gitte bytes inn i et gitt array, tror det er derfor man får rare
+     * karakterer fordi enkelte karakterer lagres som mer enn én byte
+     * Har sjekket med https://stackoverflow.com/questions/13173223/huffman-coding-dealing-with-unicode
+     * og denne får akkurat samme svar for alle testfilene
+     */
     private static int[] getFrequencyArray(String pathToFile) throws IOException {
         //todo trenger sikkert ikke å lese hele fila på en gang, kan lese i løkka lenger nede
         // gjetter jeg, kanskje bruke outputstream
 
         byte[] fileContent = Files.readAllBytes(Path.of(pathToFile));
-        int[] frequencyArray = new int[fileContent.length];
+        int[] frequencyArray = new int[256];
     /*
         char gir tall fra -128 - 127, men pga hvordan overflow funker så kan man ikke
          konvertere fra negative tall til char, trenger derimot ikke å bruke annen encoding, da
@@ -56,13 +63,14 @@ public class Huffmain {
         med modulo 256 så slipper man å sjekke om byten er < 0, da man får riktig svar uansett :)
          (x + 256) mod 256 gir naturligvis x dersom x er et positivt tall
 
+        får rare bokstaver dersom man prøver å konvertere byte og byte til char, dette er fordi
+        enkelte karakterer slik som å må ha mer enn én byte, men vi lagrer jo begge bytsene så
+        det går jo bra
      */
 
         for(int j = 0; j < fileContent.length; j++){
-            frequencyArray[j] = (fileContent[j] + 256)%256;
+            frequencyArray[(fileContent[j] + 256)%256]++;
         }
-        writeToFile(new File("files/output.txt"), frequencyArray);
         return frequencyArray;
     }
-
 }
