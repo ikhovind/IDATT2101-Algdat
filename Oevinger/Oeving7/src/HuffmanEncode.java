600eac64b00cbeb640b4f64186599816e5d2d7e5
Ingebrigt Hovind <ing.hovind@hotmail.com>
2020-10-17 17:37:05 +0200

    cleanup

---
+++ Oevinger/Oeving7/src/HuffmanEncode.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.stream.IntStream;
+
+public class HuffmanEncode {
+    static final int ANT_TEGN = 256;
+    public static void main(String[] args) throws IOException {
+        compress("files/diverse.txt","files/encoded.txt");
+    }
+
+    private static void compress(String pathToFile, String pathToCompressed) throws IOException {
+        File input = new File(pathToFile);
+        File output = new File(pathToCompressed);
+        int[] frequencyArray = getFrequencyArray(input);
+        String[] encodings = HuffmanTree.getEncodingArray(frequencyArray);
+
+        byte[] inputFile = Files.readAllBytes(input.toPath());
+
+        String fileToString = "";
+        fileToString += encodings[ANT_TEGN];
+        FileWriter myWriter = new FileWriter(output.getPath());
+        for (int i = 0; i < frequencyArray.length; i++) {
+            myWriter.write(frequencyArray[i]);
+        }
+
+        myWriter.close();
+        FileOutputStream fileOutputStream = new FileOutputStream(output.getPath(), true);
+        for (byte b : inputFile) {
+            fileToString += encodings[(b + ANT_TEGN) % ANT_TEGN];
+            if (fileToString.length() % 64 == 0 && fileToString.length() > 0){
+                fileOutputStream.write(getEncodedByteArray(fileToString));
+                fileToString = "";
+            }
+        }
+
+        fileToString += encodings[ANT_TEGN];
+        //sparer til 8 bits
+        while((fileToString += "0").length() % 8 != 0);
+
+        fileOutputStream.write(getEncodedByteArray(fileToString));
+        fileOutputStream.close();
+    }
+
+    private static byte[] getEncodedByteArray(String fileToString) {
+        ArrayList<Byte> encodedBytes = new ArrayList<>();
+
+        while(fileToString.length() > 0){
+            //når man decoder så må man simpelthen ignorerer det første sifferet i hver byte som man leser
+            encodedBytes.add((byte) Integer.parseInt((fileToString.length() >= 7 ? '1' + fileToString.substring(0,7) : '1' + fileToString),2));
+            //dersom lengden er over 7 så lages det ny substring der. Hvis lengden er under 7 så har vi lagt til hele og stringen blir tom
+            fileToString = fileToString.substring(Math.min(fileToString.length(), 7));
+        }
+
+        byte[] answer = new byte[encodedBytes.size()];
+        IntStream.range(0,encodedBytes.size()).forEach(i->answer[i] = encodedBytes.get(i));
+
+        return answer;
+    }
+
+    private static int[] getFrequencyArray(File file) throws IOException {
+
+        byte[] fileContent = Files.readAllBytes(file.toPath());
+        int[] frequencyArray = new int[ANT_TEGN + 1];
+
+        //gjør om negative bytes til positive int, med modulo 256 så slipper man å sjekke om byten er positiv, da
+        //(x + 256) mod 256 gir naturligvis x dersom x er et positivt tall
+        for (int j = 0; j < file.length(); j++) {
+            frequencyArray[(fileContent[j] + ANT_TEGN) % ANT_TEGN]++;
+        }
+        frequencyArray[frequencyArray.length - 1] = 1;
+        return frequencyArray;
+    }
+}
